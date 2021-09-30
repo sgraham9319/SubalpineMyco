8585d659a1cf75100486ab7f1c5257684ce2770b
sgraham9319 <sgraham3@uw.edu>
2021-09-29 19:51:40 -0700

    Added script for mature tree colonization analysis

---
+++ Scripts/mature_tree_colonization.R
@@ -0,0 +1,37 @@
+#####################################
+# Mature tree % colonization analysis
+#####################################
+
+# Load required packages
+library(dplyr)
+library(ggpubr)
+
+# Create standard error function
+se <- function(x){sd(x, na.rm = T) / sqrt(length(na.omit(x)))}
+
+# Load mature root data
+roots <- read.csv("../Data/field/pct_colonization_mature_tree.csv")
+
+# Calculate % colonization
+roots <- roots %>%
+  mutate(pct_col = 100 * (myco_tips / total_tips))
+
+# Did forest and meadow samples differ in % colonization?
+tapply(roots$pct_col, roots$location, mean)
+tapply(roots$pct_col, roots$location, se)
+t.test(pct_col ~ location, data = roots, paired = T)
+
+# Plot data
+ggpaired(roots, x = "location", y = "pct_col", fill = "location",
+         line.color = "gray", palette = "jco",
+         xlab = "Environment", ylab = "Root-tips colonized (%)") +
+  stat_compare_means(method = "t.test", paired = TRUE, label.y.npc = "bottom")
+
+# Did forest and meadow samples differ in number morphotypes?
+tapply(roots$num_morphotypes, roots$location, mean)
+tapply(roots$num_morphotypes, roots$location, se)
+t.test(num_morphotypes ~ location, data = roots, paired = T)
+ggpaired(roots, x = "location", y = "num_morphotypes", fill = "location",
+         line.color = "gray", palette = "jco",
+         xlab = "Environment", ylab = "# morphotypes") +
+  stat_compare_means(paired = TRUE, label.y.npc = "bottom")
